a1101247db34d88df575cf4cc9eef3e8ef2f7259
PedroVasque <pr529017@gmail.com>
2024-08-27 22:18:48 -0300

    python web

---
+++ pepe.py
@@ -0,0 +1,14 @@
+nome = input("Digite um nome")
+
+def saudacao(nome):
+    print(f"Bem-vindo {nome}")
+saudacao()
+
+
+def soma (n1, n2):
+    return(n1 + n2)
+n1 = int(input("digite um numero"))
+n2 = int(input("digite outro número"))
+print(soma(n1,n2))
+
+
